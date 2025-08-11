75432cd9df9596b0b0290aee13276809ccbe7a0b
Samridhi Gumber <samridhigumber05@gmail.com>
2025-08-11 17:32:45 +0530

    Create variables.java

---
+++ basics/variables.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        String name ="Samridhi Gumber";
+        int age =19;
+        double price=56.45;
+        int a=25;
+        int b=30;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(2*(a+b));   
+    }
+}
