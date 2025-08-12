df647084d8961d3107f72e64d762f5170e85102e
Samridhi Gumber <samridhigumber05@gmail.com>
2025-08-13 01:21:56 +0530

    Create Number upto n.java

---
+++ loops/Number upto n.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n;i++){
+            System.out.println(i);
+        }
+     
+    }
+}
+// output
+// 5
+  
+// 1 
+// 2
+// 3
+// 4
+//5
