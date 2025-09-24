d369d1bfacab6490016f49ce4e6ccbcf859416cd
Samridhi Gumber <samridhigumber05@gmail.com>
2025-09-24 12:39:10 +0530

    Create even-odd.java

---
+++ Bit Manipulation/even-odd.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        int a=21;
+        if((a&1) == 0) System.out.print("True"); // check if last bit  is 1 or 0, if 1->odd, 0->even
+        else System.out.print("False");
+    }
+}
