ebfa2edbb426c1cd27e135c0628843c1f99008ca
Samridhi Gumber <samridhigumber05@gmail.com>
2025-09-25 11:58:31 +0530

    Update Pow of two.java

--- Bit Manipulation/Pow of two.java
+++ Bit Manipulation/Pow of two.java
@@ -1,17 +1,28 @@
+// MTHOD1
+// class Main {
+//     public static boolean isPowOfTwo(int n){
+//                 if(n<=0) return false;
+//                 while(n > 1){
+//                     if((n & 1) == 1) return false;
+//                     n = n>> 1;
+//                 }
+//                 return true;
+
+//     }
+//     public static void main(String[] args) {
+//         int n=32;    
+        
+//         System.out.println(isPowOfTwo(n) ? true : false);
+//     }
+// }
+
+// METHOD2 (pROPERTY)
 
 class Main {
-    public static boolean isPowOfTwo(int n){
-                if(n<=0) return false;
-                while(n > 1){
-                    if((n & 1) == 1) return false;
-                    n = n>> 1;
-                }
-                return true;
-
-    }
     public static void main(String[] args) {
-        int n=32;    
-        
-        System.out.println(isPowOfTwo(n) ? true : false);
+        int n = 12;
+        String result = (n > 0 && (n & (n - 1)) == 0) ? "TRUE": "FALSE";
+
+        System.out.println(result);
     }
 }
